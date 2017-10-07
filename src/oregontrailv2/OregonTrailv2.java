90c27f8670466cdfe3e5e151a0d1b5b7f0dc199f
Casper <Casper@LAPTOP-HQ9MS47P>
2017-10-07 22:07:51 +1300

    test run for inventory and landmark classes

--- src/oregontrailv2/OregonTrailv2.java
+++ src/oregontrailv2/OregonTrailv2.java
@@ -7,6 +7,10 @@ package oregontrailv2;
 
 //import byui.cit260.oregonTrail.model.Player;
 
+import byui.cit260.oregonTrail.model.Inventory;
+import byui.cit260.oregonTrail.model.LandMark;
+
+
 
 /**
  *
@@ -19,7 +23,24 @@ public class OregonTrailv2 {
      */
     public static void main(String[] args) {
 
-        
+    Inventory stock = new Inventory(); //Casper  
+    
+    stock.setStockType("honey");
+    stock.setStockAmount(10);
+    stock.setRequiredAmount(5);
+    stock.setStockPoint("Missouri");
+    
+    System.out.println(stock);
+    
+    
+    LandMark feature = new LandMark();  //Casper
+    
+    feature.setDescription("Settlement");
+    feature.setTravelTime(7);
+    feature.setBlockedLocation(false);
+    feature.setDisplaySymbol("open");
+    
+    System.out.println(feature);
     }
     
 }
