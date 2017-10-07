7fe6ba06b468a7a464c4a8e3b019abe58ea39575
Casper <Casper@LAPTOP-HQ9MS47P>
2017-10-07 21:21:35 +1300

    created landMark class

--- src/byui/cit260/oregonTrail/model/Inventory.java
+++ src/byui/cit260/oregonTrail/model/Inventory.java
@@ -27,7 +27,7 @@ private String stockPoint;
     private Player player;
     private Game game;
 
-    public Game getGame() {
+    public Game getGame() {   //1..1 relationship
         return game;
     }
 
