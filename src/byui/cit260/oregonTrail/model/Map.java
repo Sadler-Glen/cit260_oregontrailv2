1639579b65f43968847e5543049e89e8122361be
adm-achina <adm-achina@BFSGLITLP.bluefs.com.gh>
2017-10-09 17:34:14 +0000

    Felix

---
+++ src/byui/cit260/oregonTrail/model/Map.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.model;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author adm-achina
+ */
+public class Map {
+    
+        
+    // variables
+    private int rowCount;
+    private int columnCount;
+    private int currentRow;
+    private int currentColumn;
+    private String currentScene;
+    private Location[][] locations;
+    private Location currentLocation;
+
+    // constructor function
+    public Map() {
+    }
+
+    public Map(int rowCount, int columnCount) {
+        if (rowCount < 1 || columnCount < 1) {
+            System.out.println("The number of rows and columns must be > zero");
+            return;
+        }
+        
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        
+        // create 2-D array for Location objects
+        this.locations = new Location[rowCount][columnCount];
+        
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                // create and initializer new Location object instance
+                Location location = new Location();
+                location.setColumn(column);
+                location.setRow(row);
+                location.setVisited(false);
+                
+                // iterate over actors and add to location if matching point found
+                outerloop:
+                for (Actor actor : Actor.values()) {
+                    if (actor.getCoordinates().x == column && actor.getCoordinates().y == row) {
+                        location.setActor(actor);
+                        break outerloop;
+                    }
+                }
+                
+                // assign the Location object to the current position in array
+                locations[row][column] = location;
+            }
+        }
+    }
+
+    // getters and setters
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(int currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public int getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public void setCurrentColumn(int currentColumn) {
+        this.currentColumn = currentColumn;
+    }
+
+    public String getCurrentScene() {
+        return currentScene;
+    }
+
+    public void setCurrentScene(String currentScene) {
+        this.currentScene = currentScene;
+    }
+    
+    public Location[][] getLocations() {
+        return locations;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    // hashcode function
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + this.rowCount;
+        hash = 97 * hash + this.columnCount;
+        hash = 97 * hash + this.currentRow;
+        hash = 97 * hash + this.currentColumn;
+        hash = 97 * hash + Objects.hashCode(this.currentScene);
+        return hash;
+    }
+
+    // equals function
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (this.columnCount != other.columnCount) {
+            return false;
+        }
+        if (this.currentRow != other.currentRow) {
+            return false;
+        }
+        if (this.currentColumn != other.currentColumn) {
+            return false;
+        }
+        if (!Objects.equals(this.currentScene, other.currentScene)) {
+            return false;
+        }
+        return true;
+    }
+
+    // toString Function
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + ", currentRow=" + currentRow + ", currentColumn=" + currentColumn + ", currentScene=" + currentScene + '}';
+    }
+      
+
+
+
+}
+    
+
