b4a398228cacec540d32143bd1f9ca6ad46c42e1
Casper <Casper@LAPTOP-HQ9MS47P>
2017-10-18 22:02:15 +1300

    Updated LandMark class, missing getter and setter for the inventory attribute.

---
+++ src/byui/cit260/oregonTrail/control/HealthLevelControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.control;
+
+/**
+ *
+ * @author Casper
+ */
+public class HealthLevelControl {
+    public double calcRemainingFood (double noOfPeople, double noDaysTravelled, double rationType, double initialFood){
+        double remainingFood;
+        
+        if (noOfPeople <1 || noDaysTravelled <0 ) {
+            return -1;
+        }
+        
+        if (initialFood > 2000){
+            return -1;
+        }
+        remainingFood = initialFood - (noOfPeople * noDaysTravelled * rationType);
+                return remainingFood;
+    }
+    
+}
