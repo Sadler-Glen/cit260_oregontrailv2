73975dda35ae5f85e18869b304c9de117426fa8a
Casper <Casper@LAPTOP-HQ9MS47P>
2017-10-19 22:06:48 +1300

    Updated LandMark class, missing getter and setter for the inventory attribute.

---
+++ src/byui/cit260/oregonTrail/control/RiverCrossing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.control;
+
+/**
+ *
+ * @author Casper
+ */
+public class RiverCrossing {
+    public int riverCrossing (int river, int crossingType) {
+        int success = 1;
+        
+        if (river == 2 && crossingType == 1) {
+            return -1;
+        }
+        
+        if (river == 3 && crossingType == 1){
+            return -1;
+        }
+        
+        if (river == 3 && crossingType == 2){
+            return -1;
+        }
+        
+        if (river == 1 && crossingType == 1 - 4){
+            return 1;
+        }
+        
+        return success;
+    }
+}
