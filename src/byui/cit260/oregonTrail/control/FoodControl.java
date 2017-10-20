d99436c7e7466ad6c62778b0bfcd28f6ffdfa7a8
sadss <sadss@GS-TOSH-P50C00E>
2017-10-20 16:04:10 +1300

    20/10/2017 07:14  Team Assignment FoodControl 100%

---
+++ src/byui/cit260/oregonTrail/control/FoodControl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregonTrail.control;
+
+/**
+ *
+ * @author sadss
+ */
+public class FoodControl {
+    public double calcRemainingFood(double noOfPeople, double noDaysTravelled, 
+        
+            double rationType, double initialFood){
+        double remainingFood;
+        if(noOfPeople < 1 || noDaysTravelled < 0  || initialFood < 0){ // test for invalid cases
+            return -1;
+        }
+        if(initialFood == 0 || initialFood > 2000 ){ // test for boundary cases
+            return -1;
+        } else {
+        }
+        remainingFood = initialFood - (noOfPeople * noDaysTravelled * rationType);
+        
+        return remainingFood;
+    }    
+}
